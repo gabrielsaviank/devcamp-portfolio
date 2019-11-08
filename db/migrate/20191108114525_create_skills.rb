2faf9967b12d695d683c5809b1fcdba5cac9dc89
Gabriel Savian <gabrielsavian@Gabriels-MacBook-Air.local>
2019-11-08 09:56:09 -0300

    integrated skills via model generator

---
+++ db/migrate/20191108114525_create_skills.rb
@@ -0,0 +1,11 @@
+class CreateSkills < ActiveRecord::Migration[5.0]
+  def change
+    create_table :skills do |t|
+      t.string :title
+      t.string :body
+      t.integer :percent_utilised
+
+      t.timestamps
+    end
+  end
+end
